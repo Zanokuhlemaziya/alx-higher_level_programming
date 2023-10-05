4a876ce9327e9214e47074a14d9351342b9fa92d
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-05 01:48:17 -0700

    TASK 2

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -4,9 +4,9 @@ def add(a, b):
 
     Args:
         a: first integer
-        b: second integer
+        b: second integer 
 
-        Returns:
-            The return value. a + b
-         """
-         return (a + b)
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
