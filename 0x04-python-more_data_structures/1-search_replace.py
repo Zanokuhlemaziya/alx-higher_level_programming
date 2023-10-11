eab2a0886dbaca25a345354852c2a4fdd5b095d1
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-11 10:01:51 -0700

    task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
+    new_list = []
     for iter1 in range(len(my_list)):
         if my_list[iter1] == search:
             new_list.append(replace)
