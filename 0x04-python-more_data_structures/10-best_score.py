5493af5503b1a58ff05222fc61d3e85d2eb5ee59
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-11 10:08:48 -0700

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-
-
 def best_score(a_dictionary):
-    """Returns a key with the biggest integer value."""
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if a_dictionary is not None and a_dictionary:
+        x = sorted(a_dictionary, key=(lambda key: a_dictionary[key]),
+                reverse=1)
+        return (x[0])
+    else:
         return None
-
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
-    for k, v in a_dictionary.items():
-        if v > big:
-            big = v
-            ret = k
-        return (ret)
