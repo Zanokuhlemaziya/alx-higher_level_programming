3478b23dc7230ecc084a367d332f572e6d68ffe6
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-11 10:20:52 -0700

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is not None and a_dictionary:
         x = sorted(a_dictionary, key=(lambda key: a_dictionary[key]),
-                reverse=1)
+                   reverse=1)
         return (x[0])
     else:
         return None
