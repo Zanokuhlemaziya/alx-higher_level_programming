ed7525555305a3fb3501663b4276d2214c3ee109
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-23 16:11:58 -0700

    task

--- 0x05-python-exceptions/103-tests.py
+++ 0x05-python-exceptions/103-tests.py
@@ -1,78 +1,41 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
-#include <bytesobject.h>
-#include <floatobject.h>
-#include <stdlib.h>
-#include <float.h>
+#!/usr/bin/python3 -u
 
-void print_python_float(PyObject *p)
-{
-        PyFloatObject *f = (PyFloatObject *)p;
-        double d = f->ob_fval;
-        char *str = NULL;
+import ctypes
 
-        printf("[.] float object info\n");
-        if (!PyFloat_Check(f))
-        {
-            printf("  [ERROR] Invalid Float Object\n");
-            return;
-            }
-
-        str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-        printf("  value: %s\n", str);
-        }
-
-void print_python_bytes(PyObject *p)
-{
-        long unsigned int size;
-        unsigned int i;
-        char *trying_str = NULL;
-
-        printf("[.] bytes object info\n");
-        if (!PyBytes_Check(p))
-        {
-            printf("  [ERROR] Invalid Bytes Object\n");
-            return;
-        }
-        
-        size = ((PyVarObject *)p)->ob_size;
-        trying_str = ((PyBytesObject *)p)->ob_sval;
-        printf("  size: %lu\n", size);
-        printf("  trying string: %s\n", trying_str);
-        if (size < 10)
-        printf("  first %lu bytes:", size + 1);
-        else
-        printf("  first 10 bytes:");
-        for (i = 0; i <= size && i < 10; i++)
-        printf(" %02hhx", trying_str[i]);
-        printf("\n");
-}
-
-void print_python_list(PyObject *p)
-{
-        long unsigned int size;
-        unsigned int i;
-        PyListObject *list = (PyListObject *)p;
-        const char *type;
-
-        printf("[*] Python list info\n");
-        if (!PyList_Check(list))
-        {
-            printf("  [ERROR] Invalid List Object\n");
-            return;
-        }
-
-        size = ((PyVarObject *)p)->ob_size;
-        printf("[*] Size of the Python List = %lu\n", size);
-        printf("[*] Allocated = %lu\n", list->allocated);
-        for (i = 0; i < size; i++)
-        {
-            type = (list->ob_item[i])->ob_type->tp_name;
-            printf("Element %i: %s\n", i, type);
-            if (!strcmp(type, "bytes"))
-            print_python_bytes(list->ob_item[i]);
-            if (!strcmp(type, "float"))
-            print_python_float(list->ob_item[i]);
-        }
-}
+lib = ctypes.CDLL('./libPython.so')
+lib.print_python_list.argtypes = [ctypes.py_object]
+lib.print_python_bytes.argtypes = [ctypes.py_object]
+lib.print_python_float.argtypes = [ctypes.py_object]
+s = b"Hello"
+lib.print_python_bytes(s);
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
+lib.print_python_bytes(b);
+b = b'What does the \'b\' character do in front of a string literal?';
+lib.print_python_bytes(b);
+l = [b'Hello', b'World']
+lib.print_python_list(l)
+del l[1]
+lib.print_python_list(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"School", "Betty"]
+lib.print_python_list(l)
+l = []
+lib.print_python_list(l)
+l.append(0)
+lib.print_python_list(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list(l)
+l.pop()
+lib.print_python_list(l)
+l = ["School"]
+lib.print_python_list(l)
+lib.print_python_bytes(l);
+f = 3.14
+lib.print_python_float(f);
+l = [-1.0, -0.1, 0.0, 1.0, 3.14, 3.14159, 3.14159265, 3.141592653589793238462643383279502884197169399375105820974944592307816406286]
+print(l)
+lib.print_python_list(l);
+lib.print_python_float(l);
+lib.print_python_list(f);
