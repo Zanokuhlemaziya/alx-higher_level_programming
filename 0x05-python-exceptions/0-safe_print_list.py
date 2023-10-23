5f4a84de537acbd7042ff8f28b94f71a3f8d7298
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-23 12:01:37 -0700

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
+    cont = 0
+    for iter1 in range(0, x):
         try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
+            print(my_list[iter1], end='')
+            cont += 1
+        except (IndexError):
+            pass
         print("")
-        Return
+        return cont
