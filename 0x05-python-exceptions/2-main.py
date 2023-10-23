7d69a6aa023700d7755855edd515a026c42237f6
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-23 13:11:48 -0700

    updated

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -3,6 +3,7 @@ safe_print_list_integers = \
         __import__('2-safe_print_list_integers').safe_print_list_integers
 
 my_list = [1, 2, 3, 4, 5]
+
 nb_print = safe_print_list_integers(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
 
@@ -12,4 +13,3 @@ print("nb_print: {:d}".format(nb_print))
 
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-
