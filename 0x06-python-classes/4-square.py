9381905ef17cbc1b4a7f8976cc5c0daecc720d9d
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-24 09:43:57 -0700

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
-
 class Square:
-    """Initialize a new square.
-    Args:
-         size (int): The size of the new square.
-    """
-    self.size = size
+    """Represent a square."""
+
+    def __init__(self, size=0):
+       """Initialize a new square.
+       Args:
+             size (int): The size of the new square.
+       """
+       self.size = size
 
     @property
     def size(self):
