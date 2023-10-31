015387e4072aff06f681565f31f619e37ad853b8
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-10-31 12:06:58 -0700

    task

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# 6-rectangle.py
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle.
+    Attributes:
+              number_of_instances (int): The number of Rectangle instances.
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """Get/set the height of the Rectangle."""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+
+        def area(self):
+            """Return the area of the Rectangle."""
+            return (self.__width * self.__height)
