883ab720e2fe97ce4208dc5560b62b737a4e8a7f
Zanokuhlemaziya <Zanokuhlemaziya.com>
2023-11-06 06:43:54 -0800

    task

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,11 +7,11 @@ class Square(Rectangle):
     """Represent a square."""
 
     def __init__(self, size):
-    """Initialize a new square.
+        """Initialize a new square.
 
-    Args:
-        size (int): The size of the new square.
-    """
-    self.integer_validator("size", size)
-    super().__init__(size, size)
-    self.__size = size
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
