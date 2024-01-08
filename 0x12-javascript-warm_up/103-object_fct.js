b80dddfb9705c4f39841b4f4fdfbe824e2407d19
Zanokuhlemaziya <zaza.zulu9.gmail.com>
2024-01-08 14:39:56 +0000

    task

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
-// edit existing code (insert at line 8-10) to add `incr` function
+
 const myObject = {
   type: 'object',
-  value: 12
+  value: 12,
+  incr: function () {
+    this.value++;
+  }
 };
+
 console.log(myObject);
-myObject.incr = function () {  // begin added code
-  this.value = this.value + 1;
-};  // end added code
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
