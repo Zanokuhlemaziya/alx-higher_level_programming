88749bc47e9a9f43fceaad963f74abdc464b6f0b
Zanokuhlemaziya <zaza.zulu9.gmail.com>
2024-01-09 14:27:58 +0000

    task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-console.log(require('./100-data.js').list);
-console.log((require('./100-data.js').list).map((a, b) => a * b));
+const list = require('./100-data').list;
+const newList = list.map((item, index) => item * index);
+
+console.log(list);
+console.log(newList);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFileSync(process.argv[4], fs.readFileSync(process.argv[2], 'utf8') + fs.readFileSync(process.argv[3], 'utf8'));
+const firstFile = fs.readFileSync(process.argv[2], 'utf8');
+const secondFile = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], firstFile + secondFile);
